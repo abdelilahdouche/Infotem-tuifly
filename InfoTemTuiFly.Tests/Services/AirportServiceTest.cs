f6f9bbdc69dd8202536886e5d78ba8d971c9ea69
abdelilah douche <abde.dou@hotmail.fr>
2019-02-10 20:21:56 +0100

    Services Unit Test

---
+++ InfoTemTuiFly.Tests/Services/AirportServiceTest.cs
@@ -0,0 +1,92 @@
+﻿using InfoTemTuiFly.Models.Entities;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace InfoTemTuiFly.Tests.Services
+{
+    /// <summary>
+    /// Airport Service test
+    /// </summary>
+    public class AirportServiceTest : ServiceBaseTest
+    {
+        [Test]
+        public void AirportServiceTest_GetAll()
+        {
+            //Arrange
+            //Act
+            var result = _airportService.GetAll();
+
+            //Assert
+            Assert.IsNotNull(result);
+            ((IQueryable<Airport>)_appContext.Airport).Received().ToList();
+        }
+
+        [Test]
+        public void AirportServiceTest_GetById()
+        {
+            //Arrange
+            //Act
+            var result = _airportService.GetById(1);
+
+            //Assert
+            _appContext.Airport.Received().Find(Arg.Any<int>());
+        }
+
+        [Test]
+        public void AirportServiceTest_Save_Null()
+        {
+            //Arrange
+            Airport airport = null;
+            //Act
+            var result = Assert.Throws<ArgumentNullException>(() => _airportService.Save(airport));
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotEmpty(result.Message);
+        }
+
+        [Test]
+        public void AirportServiceTest_Save_New()
+        {
+            //Arrange
+            var airport = new Airport() { Id = 0, Name = "Airport" };
+            //Act
+            var result = _airportService.Save(airport);
+
+            //Assert
+            Assert.NotNull(result);
+            _appContext.Airport.Received().Add(Arg.Any<Airport>());
+            _appContext.DidNotReceive().Update(Arg.Any<Airport>());
+        }
+
+        [Test]
+        public void AirportServiceTest_Save_Exist()
+        {
+            //Arrange
+            var airport = new Airport() { Id = 1, Name = "Airport" };
+            //Act
+            var result = _airportService.Save(airport);
+
+            //Assert
+            Assert.NotNull(result);
+            _appContext.Airport.DidNotReceive().Add(Arg.Any<Airport>());
+            _appContext.Received().Update(Arg.Any<Airport>());
+        }
+
+        [Test]
+        public void AirportServiceTest_Delete()
+        {
+            //Arrange
+            var airport = new Airport() { Id = 1, Name = "Airport" };
+            _airportService.GetById(Arg.Is<int>(a => a > 0)).Returns(airport);
+            //Act
+            var result = _airportService.Delete(1);
+
+            //Assert
+            Assert.IsTrue(result);
+            _appContext.Airport.Received().Remove(airport);
+        }
+    }
+}
---
+++ InfoTemTuiFly.Tests/Services/FlightServiceTest.cs
@@ -0,0 +1,89 @@
+﻿using InfoTemTuiFly.Models.Entities;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace InfoTemTuiFly.Tests.Services
+{
+    internal class FlightServiceTest : ServiceBaseTest
+    {
+        [Test]
+        public void FlightServiceTest_GetAll()
+        {
+            //Arrange
+            //Act
+            var result = _flightService.GetAll();
+
+            //Assert
+            Assert.IsNotNull(result);
+            ((IQueryable<Flight>)_appContext.Flight).Received().ToList();
+        }
+
+        [Test]
+        public void FlightServiceTest_GetById()
+        {
+            //Arrange
+            //Act
+            var result = _flightService.GetById(1);
+
+            //Assert
+            ((IQueryable<Flight>)_appContext.Flight).ReceivedWithAnyArgs().SingleOrDefault();
+        }
+
+        [Test]
+        public void FlightServiceTest_Save_Null()
+        {
+            //Arrange
+            Flight Flight = null;
+            //Act
+            var result = Assert.Throws<ArgumentNullException>(() => _flightService.Save(Flight));
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotEmpty(result.Message);
+        }
+
+        [Test]
+        public void FlightServiceTest_Save_New()
+        {
+            //Arrange
+            var Flight = new Flight() { Id = 0, Name = "Flight" };
+            //Act
+            var result = _flightService.Save(Flight);
+
+            //Assert
+            Assert.NotNull(result);
+            _appContext.Flight.Received().Add(Arg.Any<Flight>());
+            _appContext.DidNotReceive().Update(Arg.Any<Flight>());
+        }
+
+        [Test]
+        public void FlightServiceTest_Save_Exist()
+        {
+            //Arrange
+            var Flight = new Flight() { Id = 1, Name = "Flight" };
+            //Act
+            var result = _flightService.Save(Flight);
+
+            //Assert
+            Assert.NotNull(result);
+            _appContext.Flight.DidNotReceive().Add(Arg.Any<Flight>());
+            _appContext.Received().Update(Arg.Any<Flight>());
+        }
+
+        [Test]
+        public void FlightServiceTest_Delete()
+        {
+            //Arrange
+            var Flight = new Flight() { Id = 1, Name = "Flight" };
+            _flightService.GetById(Arg.Is<int>(a => a > 0)).Returns(Flight);
+            //Act
+            var result = _flightService.Delete(1);
+
+            //Assert
+            Assert.IsTrue(result);
+            _appContext.Flight.Received().Remove(Flight);
+        }
+    }
+}
